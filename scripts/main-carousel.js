46f656a8028ad944c7acb9d5c8e82105b2753997
Danilo Fontes <slipdan@hotmail.com>
2021-05-11 19:06:37 -0300

    Add images and create js animation to carousel

---
+++ scripts/main-carousel.js
@@ -0,0 +1,15 @@
+
+const arrowLeft = document.querySelector('.button-arrow.-left');
+const arrowRight = document.querySelector('.button-arrow.-right');
+const elements = document.querySelector('.elements');
+let move = 100;
+
+arrowLeft.addEventListener('click', function() {
+    move = move - 100;
+    elements.style = `transform: translateX(${move}px)`;
+})
+
+arrowRight.addEventListener('click', function(){
+    move = move + 100;
+    elements.style = `transform: translateX(${move}px)`;
+})
